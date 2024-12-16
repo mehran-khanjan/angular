2f0487adb35cbabaaa83c509b2c94d3a75c32b3f
unknown <mehrankh1913@gmail.com>
2024-12-16 21:05:20 +0330

    my portfolio

---
+++ blog/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA9mjU514853B13y--vN_PdlAM6r2ZHZm4',
+    authDomain: 'angular-blog-f1e36.firebaseapp.com',
+    projectId: 'angular-blog-f1e36',
+    storageBucket: 'angular-blog-f1e36.appspot.com',
+    messagingSenderId: '912748235898',
+    appId: '1:912748235898:web:11f6e864afa010e0cc7fc3',
+  },
+};
